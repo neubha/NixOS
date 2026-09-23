db78c75c6a8563c26579f43f3d7ce5d01366b226
Shubha Sandesh Sharma Neupane <indivshubha@gmail.com>
2026-09-23 14:31:00 +0545

    First Flake Commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations.helheim = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        ./configuration.nix
+      ];
+    };
+
+    packages.x86_64-linux = {
+      hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      default = self.packages.x86_64-linux.hello;
+    };
+  };  
+
+}
