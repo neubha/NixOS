376cd0caeaba01985ecc71daaddabfd6e77e72bb
Shubha Sandesh Sharma Neupane <indivshubha@gmail.com>
2026-09-24 08:37:43 +0545

    DE change Installed Plasma, cause well seems GNOME is pretty useless, changed the hostname

--- configuration.nix
+++ configuration.nix
@@ -7,14 +7,14 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      /etc/nixos/hardware-configuration.nix
+      ./hardware-configuration.nix
     ];
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  networking.hostName = "helheim"; # Define your hostname.
+  networking.hostName = "NixOS"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -31,8 +31,18 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+  #services.displayManager.gdm.enable = true;
+  #services.desktopManager.gnome.enable = true;
+
+  #Enable the Plasma Desktop Environment
+  services.desktopManager.plasma6.enable = true;
+  services.displayManager.plasma-login-manager.enable = true;
+
+  #Dont install these packages
+  #environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    #konsole
+    #kate
+  #];
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -73,102 +83,65 @@
   programs.firefox.enable = true;
 
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  #nixpkgs.config.allowUnfree = true;
+
+  # Niri Setup
+  #programs.niri.enable = true;
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
-     vim
-     git
-     fastfetch
-     alacritty
-     zed-editor
-     btop
-     bun
-          
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
+    git
+    fastfetch
+    alacritty
+    zed-editor
+    btop
+    bun
+    github-cli
+    niri
+    xwayland-satellite
   ];
 
-
   # Some programs need SUID wrappers, can be configured further or are
-
   # started in user sessions.
-
   # programs.mtr.enable = true;
-
   # programs.gnupg.agent = {
-
   #   enable = true;
-
   #   enableSSHSupport = true;
-
   # };
-
 
   # List services that you want to enable:
 
-
   # Enable the OpenSSH daemon.
-
   # services.openssh.enable = true;
 
-
   # Open ports in the firewall.
-
   # networking.firewall.allowedTCPPorts = [ ... ];
-
   # networking.firewall.allowedUDPPorts = [ ... ];
-
   # Or disable the firewall altogether.
-
   # networking.firewall.enable = false;
 
-
   # Copy the NixOS configuration file and link it from the resulting system
-
   # (/run/current-system/configuration.nix). This is useful in case you
-
   # accidentally delete configuration.nix.
-
   # system.copySystemConfiguration = true;
 
-
   # This option defines the first version of NixOS you have installed on this particular machine,
-
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-
   #
-
   # Most users should NEVER change this value after the initial install, for any reason,
-
   # even if you've upgraded your system to a new NixOS release.
-
   #
-
   # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-
   # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-
   # to actually do that.
-
   #
-
   # This value being lower than the current NixOS release does NOT mean your system is
-
   # out of date, out of support, or vulnerable.
-
   #
-
   # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-
   # and migrated your data accordingly.
-
   #
-
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-
   system.stateVersion = "26.05"; # Did you read the comment?
-
-
-} 
+}
--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   };
 
   outputs = { self, nixpkgs, ... }: {
-    nixosConfigurations.helheim = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.NixOS = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
 
       modules = [
